7ad672396dab88dbba0155473ce0b3689006afea
ParasPalli <paraspalli1234@gmail.com>
2026-04-12 13:59:09 +0530

    Added SpringSecurity Config

---
+++ src/main/java/com/example/jounaling_app/config/SpringSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.jounaling_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/journal/**").authenticated()
+        .anyRequest().permitAll())
+        .formLogin(form -> form.disable());
+
+    return http.build();
+  }
+
+}
