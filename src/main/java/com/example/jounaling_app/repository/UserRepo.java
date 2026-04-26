f9cadfa16ea7aa61f8e1a0a1d387e565425b2c67
ParasPalli <paraspalli1234@gmail.com>
2026-04-26 12:48:04 +0530

    Added Email Service

--- src/main/java/com/example/jounaling_app/repository/UserRepo.java
+++ src/main/java/com/example/jounaling_app/repository/UserRepo.java
@@ -9,3 +9,27 @@ public interface UserRepo extends MongoRepository<UserEntity, ObjectId> {
   // Query Method
   UserEntity findByUsername(String username);
 }
+
+
+// class UserRepoCriteria {
+  
+//   @Autowired
+//   private MongoTemplate mongoTemplate;
+  
+// public List<UserEntity> getUsersByName {
+//   Query query = new Query();
+
+//   query.addCriteria(Criteria.where("name").is("paras"));
+//   query.addCriteria(Criteria.where("email").exists(true));
+
+//   // --- Adding Boolean Conditions
+//   Criteria criteria = new Criteria();
+//   query.addCriteria(criteria.andOperator(
+//     Criteria.where("name").is("paras"),
+//     Criteria.where("email").exists(true),
+//   ));
+
+//   List<UserEntity> users = mongoTemplate.find(query, UserEntity.class);
+// }
+
+// }
