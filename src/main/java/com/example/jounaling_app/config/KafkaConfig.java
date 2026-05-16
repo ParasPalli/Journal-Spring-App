9a0999e97d17f04debe6aa51dee29fe7d43b12fe
ParasPalli <paraspalli1234@gmail.com>
2026-05-16 13:36:26 +0530

    Added Certificates, Properties and Pacakage of Kafka

---
+++ src/main/java/com/example/jounaling_app/config/KafkaConfig.java
@@ -0,0 +1,13 @@
+package com.example.jounaling_app.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+  
+  // @Bean
+  // public NewTopic cornTopic() {
+  //   return TopicBuilder.name("corn-topic").partitions(1).build();
+  // } 
+
+}
