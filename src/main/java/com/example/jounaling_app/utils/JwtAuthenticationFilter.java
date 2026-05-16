15091633622b9f6e663f54ece576bf98c2411377
ParasPalli <paraspalli1234@gmail.com>
2026-05-16 16:55:59 +0530

    Solved circular references Issue in Jwt Filter Class

--- src/main/java/com/example/jounaling_app/utils/JwtAuthenticationFilter.java
+++ src/main/java/com/example/jounaling_app/utils/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.jounaling_app.utils;
 
 import java.io.IOException;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,14 +15,17 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 
 @Component
-@RequiredArgsConstructor // Injects Autowired Using Lambok
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JWTUtils jwtUtils;
   private final UserDetailsService userDetailsService;
+
+  public JwtAuthenticationFilter(JWTUtils jwtUtils, @Lazy UserDetailsService userDetailsService) {
+    this.jwtUtils = jwtUtils;
+    this.userDetailsService = userDetailsService;
+  }
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
